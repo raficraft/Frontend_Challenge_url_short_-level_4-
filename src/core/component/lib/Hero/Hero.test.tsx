99b2b574989a776d4c589be347c723374b927f7d
raficraft <raficraft@gmail.com>
2022-06-12 21:44:55 +0200

    add test for hero component

---
+++ src/core/component/lib/Hero/Hero.test.tsx
@@ -0,0 +1,28 @@
+import "@testing-library/jest-dom";
+import { getByText, render } from "@testing-library/react";
+import Hero from "./Hero";
+
+describe("Navigation component and items is rendered", () => {
+  test(" Navigation should be rendered", () => {
+    const { container } = render(<Hero></Hero>);
+    expect(container).toBeInTheDocument();
+  });
+  test("Navigation items should be rendered", () => {
+    const { getByText } = render(<Hero></Hero>);
+
+    expect(getByText(/features/i)).toBeInTheDocument();
+    expect(getByText(/build your/i)).toBeInTheDocument();
+    expect(getByText(/get started/i)).toBeInTheDocument();
+  });
+
+  test("Navigation items should be rendered", () => {
+    const { getByText, getByPlaceholderText } = render(
+      <Hero>
+        <UrlShort></UrlShort>
+      </Hero>
+    );
+
+    expect(getByPlaceholderText(/shorten/i)).toBeInTheDocument();
+    expect(getByText(/shorten/i)).toBeInTheDocument();
+  });
+});
